16355c7bcf86f051f19ed2b249bb24afef49c7e4
Sean Klijn <77756@glr.nl>
2016-11-11 10:22:50 +0100

    Controllers

--- resources/views/Branches/index.blade.php
+++ resources/views/Branches/index.blade.php
@@ -2,26 +2,30 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <h1>All Branches</h1>
-            <ul class="list-group">
-                @foreach($branches as $branche)
-                    <li class="list-group-item"  onclick="location.href='/branche/{{ $branche->id }}'" onmouseover=" style='cursor:pointer';">
-                        {{$branche->name}}
-                    </li>
-                @endforeach
-            </ul>
-            <hr>
-            <h3>Add a new branche</h3>
-            <form action="/newbranche" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group">
-                    <textarea name="name" class="form-control" required></textarea>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Add Branche</button>
-                </div>
-            </form>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h1>All Branches</h1></div>
+                <div class="panel-body">
+                    <h2>Add a new branche</h2>
+                    <form action="/newbranche" method="post">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control" required></input>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Add Branche</button>
+                        </div>
+                    </form>
+                    <hr>
+                    <ul class="list-group">
+                        @foreach($branches as $branche)
+                            <li class="list-group-item"  onclick="location.href='/branche/{{ $branche->id }}'" onmouseover=" style='cursor:pointer';">
+                                {{$branche->name}}
+                            </li>
+                        @endforeach
+                    </ul>
         </div>
     </div>
+    </div>
+</div>
 @endsection
